0074d0322279d657a3abfc80972e3785b20ede4e
Yaroslav Kravets <yar.kravets@ukr.net>
2021-07-10 22:10:39 +0300

    create project, add model Airport

---
+++ examen/src/main/java/models/Airport.java
@@ -0,0 +1,81 @@
+package models;
+
+public class Airport {
+
+    private int id;
+    private String name;
+    private String icao_code;
+    private String iata_code;
+    private String status;
+    private double longitude;
+    private double latitude;
+
+    public Airport() {
+    }
+
+    public Airport(int id, String name, String icao_code, String iata_code, String status, double longitude, double latitude) {
+        this.id = id;
+        this.name = name;
+        this.icao_code = icao_code;
+        this.iata_code = iata_code;
+        this.status = status;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcao_code() {
+        return icao_code;
+    }
+
+    public void setIcao_code(String icao_code) {
+        this.icao_code = icao_code;
+    }
+
+    public String getIata_code() {
+        return iata_code;
+    }
+
+    public void setIata_code(String iata_code) {
+        this.iata_code = iata_code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
